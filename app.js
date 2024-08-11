ffc44e3cc1929648a7b30cd4798a8dc976f1fbbb
Carlos Gabriel Carreño <carlosgabrielcarrenio@gmail.com>
2024-08-11 19:25:37 -0300

    creating the general structure of the calculator

---
+++ app.js
@@ -0,0 +1,5 @@
+// Esta funcion convierte los datos de entrada de tipo string en tipo number y los retorna como un obj
+// con sus datos separados por jerarquia.
+function convertDataToNumbers(data) {
+  let value1, value2;
+}
