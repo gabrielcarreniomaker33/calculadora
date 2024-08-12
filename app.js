64695d5e651adabe287f94f0033680ebb41fadb0
Carlos Gabriel Carreño <carlosgabrielcarrenio@gmail.com>
2024-08-12 15:28:25 -0300

    working with logic in JS

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-// Esta funcion convierte los datos de entrada de tipo string en tipo number y los retorna como un obj
-// con sus datos separados por jerarquia.
-function convertDataToNumbers(data) {
-  let value1, value2;
+function readInput() {
+  let valueInput = document.getElementById("input-data");
+  return valueInput.value;
 }
+
+function add() {
+  alert(readInput());
+}
+
+function main() {
+  //
+}
+
+main();
